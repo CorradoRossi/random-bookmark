c015e19aae807d7af54fa7c1b07cae2edc78ac0d
Rossi Meacham <rossimeacham@gmail.com>
2024-08-05 15:00:34 -0400

    newtab stuff

--- newtab.js
+++ newtab.js
@@ -1,6 +1,12 @@
 chrome.storage.local.get("currentHomepage", (result) => {
-  if (result.currentHomepage) {
+  if (result.currentHomepage && result.refreshInterval !== "everyNewTab") {
     window.location.href = result.currentHomepage;
+  } else if (result.refreshInterval === "everyNewTab") {
+    chrome.runtime.sendMessage({ action: "getNewRandom" }, (response) => {
+      if (response && response.currentHomepage) {
+        window.location.href = response.currentHomepage;
+      }
+    });
   } else {
     document.body.innerHTML =
       "<p>No bookmarks found. Please add some bookmarks and reload.</p>";

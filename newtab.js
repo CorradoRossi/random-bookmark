ccd151ff89c37e71e8fe7604207a8f01fecc8caf
Rossi Meacham <rossimeacham@gmail.com>
2024-08-05 15:09:45 -0400

    console logs

--- newtab.js
+++ newtab.js
@@ -1,9 +1,13 @@
 chrome.storage.local.get("currentHomepage", (result) => {
+  console.log("result 1", result);
   if (result.currentHomepage && result.refreshInterval !== "everyNewTab") {
+    console.log("result.currentHomepage 2", result.currentHomepage);
     window.location.href = result.currentHomepage;
   } else if (result.refreshInterval === "everyNewTab") {
+    console.log("result.refreshInterval", result.refreshInterval);
     chrome.runtime.sendMessage({ action: "getNewRandom" }, (response) => {
       if (response && response.currentHomepage) {
+        console.log("response.currentHomepage 3", response.currentHomepage);
         window.location.href = response.currentHomepage;
       }
     });

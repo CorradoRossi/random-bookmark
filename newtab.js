813443f0d6e0121e89258a53b4cd91c41e309968
Rossi Meacham <rossimeacham@gmail.com>
2024-08-05 22:44:12 -0400

    fixed it

--- newtab.js
+++ newtab.js
@@ -1,18 +1,17 @@
-chrome.storage.local.get("currentHomepage", (result) => {
-  console.log("result 1", result);
-  if (result.currentHomepage && result.refreshInterval !== "everyNewTab") {
-    console.log("result.currentHomepage 2", result.currentHomepage);
-    window.location.href = result.currentHomepage;
-  } else if (result.refreshInterval === "everyNewTab") {
-    console.log("result.refreshInterval", result.refreshInterval);
-    chrome.runtime.sendMessage({ action: "getNewRandom" }, (response) => {
-      if (response && response.currentHomepage) {
-        console.log("response.currentHomepage 3", response.currentHomepage);
-        window.location.href = response.currentHomepage;
-      }
-    });
-  } else {
-    document.body.innerHTML =
-      "<p>No bookmarks found. Please add some bookmarks and reload.</p>";
+chrome.storage.local.get(
+  ["currentHomepage", "refreshInterval", "lastUpdated"],
+  (result) => {
+    if (result.currentHomepage && result.refreshInterval !== "everyNewTab") {
+      window.location.href = result.currentHomepage;
+    } else if (result.refreshInterval === "everyNewTab") {
+      chrome.runtime.sendMessage({ action: "getNewRandom" }, (response) => {
+        if (response && response.currentHomepage) {
+          window.location.href = response.currentHomepage;
+        }
+      });
+    } else {
+      document.body.innerHTML =
+        "<p>No bookmarks found. Please add some bookmarks and reload.</p>";
+    }
   }
-});
+);
